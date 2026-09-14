f052442f1691aee74baf060fa97a81da2bdf8531
NikitaDyachkov5 <nik.drylia@mail.ru>
2026-09-14 12:42:30 +0500

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,24 +1,48 @@
 class HomePage extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content class="ion-padding">
-        <div id="container">
-          <strong>Ready to create an app?</strong>
-          <p>
-            Start with Ionic
-            <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a>
-          </p>
-        </div>
-<ion-router-link href="/new">
-  <ion-button>Navigate</ion-button>
-</ion-router-link>
+            <ion-header>
+                <ion-toolbar>
+                    <ion-title>Blank</ion-title>
+                </ion-toolbar>
+            </ion-header>
 
-    `;
+            <ion-content class="ion-padding">
+                <div id="container">
+                    <strong>Ready to create an app?</strong>
+                    <p>
+                        Start with Ionic
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://ionicframework.com/docs/components"
+                        >
+                            UI Components
+                        </a>
+                    </p>
+                </div>
+
+                <ion-router-link href="/new">
+                    <ion-button>Navigate</ion-button>
+                </ion-router-link>
+            </ion-content>
+        `;
+
+        this.addEventListener('ionViewWillEnter', () => {
+            console.log('HomePage: ionViewWillEnter');
+        });
+
+        this.addEventListener('ionViewDidEnter', () => {
+            console.log('HomePage: ionViewDidEnter');
+        });
+
+        this.addEventListener('ionViewWillLeave', () => {
+            console.log('HomePage: ionViewWillLeave');
+        });
+
+        this.addEventListener('ionViewDidLeave', () => {
+            console.log('HomePage: ionViewDidLeave');
+        });
     }
 }
 
